840b200d5eaa38906fd71b77fc6738e14df22879
xnorman <xnorman@xleon.com>
2012-04-13 23:25:12 +0800

    xnorman <xnorman@xleon.com> image code

---
+++ WebContent/imagescode.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <script type="text/javascript" language="javascript">
+  function changeImg(){
+   var a=document.getElementById("mySpan");
+   a.innerHTML='<img align="middle" src="ImageServlet"/>  <a href="#" onclick="changeImg();">看不清</a>'; //会复盖“mySpan”位置
+  }
+</script> 
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>图片验证码</title>
+</head>
+<body>
+  <span id="mySpan"><img align="middle" src="ImageServlet" /><a
+   href="#" onclick="changeImg();">看不清</a> </span> 
+</body>
+</html>
---
+++ src/com/xleon/demo/piccode/PicCodeDemo.java
@@ -0,0 +1,105 @@
+package com.xleon.demo.piccode;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Random;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class PicCodeDemo
+ */
+public class PicCodeDemo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public PicCodeDemo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	// 验证码图片的宽度。
+	private int width = 60;
+	// 验证码图片的高度。
+	private int height = 20;
+
+	protected void service(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, java.io.IOException {
+		BufferedImage buffImg = new BufferedImage(width, height,
+				BufferedImage.TYPE_INT_RGB);
+		Graphics2D g = buffImg.createGraphics();
+
+		// 创建一个随机数生成器类。
+		Random random = new Random();
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, width, height);
+
+		// 创建字体，字体的大小应该根据图片的高度来定。
+		Font font = new Font("Times New Roman", Font.BOLD, 18);
+
+		// 设置字体。
+		g.setFont(font);
+
+		// 画边框。
+		g.setColor(Color.BLACK);
+		g.drawRect(0, 0, width - 1, height - 1);
+
+		// 随机产生40条干扰 线，使图象中的认证码不易被其它程序探测到。
+		g.setColor(Color.GRAY);
+		for (int i = 0; i < 40; i++) {
+			int x = random.nextInt(width);
+			int y = random.nextInt(height);
+			int xl = random.nextInt(12);
+			int yl = random.nextInt(12);
+			g.drawLine(x, y, x + xl, y + yl);
+		}
+
+		// randomCode用于保存随机产生的验证 码，以便用户登录后进行验证。
+		StringBuffer randomCode = new StringBuffer();
+		int red = 0, green = 0, blue = 0;
+
+		// 随机产生4位数字的验 证码。
+		for (int i = 0; i < 4; i++) {
+			// 得到随机产生 的验证码数字。
+			String strRand = String.valueOf(random.nextInt(10));
+			// 产生随机的颜色分量来构造颜色值，这样 输出的每位数字的颜色值都将不同。
+			red = random.nextInt(110);
+			green = random.nextInt(50);
+			blue = random.nextInt(50);
+
+			// 用随机产生的 颜色将验证码绘制到图像中。
+			g.setColor(new Color(red, green, blue));
+			g.drawString(strRand, 13 * i + 6, 16);
+
+			// 将产生的四个 随机数组合在一起。
+			randomCode.append(strRand);
+		}
+
+		// 将四位数字的验证码保存到Session中。
+		HttpSession session = req.getSession();
+		session.setAttribute("randomCode", randomCode.toString());
+
+		// 禁止图像缓存。
+		resp.setHeader("Pragma", "no-cache");
+		resp.setHeader("Cache-Control", "no-cache");
+		resp.setDateHeader("Expires", 0);
+		resp.setContentType("image/jpeg");
+
+		// 将图像输出到 Servlet输出流中。
+		ServletOutputStream sos = resp.getOutputStream();
+		ImageIO.write(buffImg, "jpeg", sos);
+		sos.close();
+
+	}
+}
