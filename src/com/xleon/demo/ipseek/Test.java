1d1768a6d8d00649025b79d530c25f09fd758008
xnorman <xnorman@xleon.com>
2012-04-15 01:30:22 +0800

    xnorman <xnorman@xleon.com> 添加IP测试代码

---
+++ src/com/xleon/demo/ipseek/Test.java
@@ -0,0 +1,10 @@
+package com.xleon.demo.ipseek;
+
+public class Test {
+	public static void main(String[] args) {
+		Test test = new Test();
+		System.out.println(test.getClass().getResource("/"));
+	}
+	
+	
+}
